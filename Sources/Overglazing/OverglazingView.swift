efd821dbbf2148f6b15d14aed39863042db2e422
Lagrange1813 <Lagrange1813@163.com>
2022-06-14 01:06:25 +0800

    auto commit

---
+++ Sources/Overglazing/OverglazingView.swift
@@ -0,0 +1,20 @@
+//
+//  OverglazingView.swift
+//  Overglazing
+//
+//  Created by 张维熙 on 2022/6/2.
+//
+
+import UIKit
+
+class OverglazingView: UITextView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
